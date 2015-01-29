c7438116bee1a07e9af63cc05d56ca54f1e4239a
jalves59 <jalves59@ford.com>
2015-01-29 08:51:35 -0200

    Added ForgotPasswordViewController on StoryBoard, created a new class for it.

---
+++ FreteList/ForgotPasswordViewController.h
@@ -0,0 +1,15 @@
+//
+//  ForgotPasswordViewController.h
+//  FreteList
+//
+//  Created by Joao Alves on 1/29/15.
+//  Copyright (c) 2015 FreteList. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgotPasswordViewController : UIViewController
+
+
+
+@end
