cba136db28431906bb55b5242c3983a6c99b4649
jalves59 <jalves59@ford.com>
2015-02-23 10:45:03 -0300

    Added FreightListDetailCell Class, and edited its labels on storyboard.

---
+++ FreteList/FreightDetailCustomCell.h
@@ -0,0 +1,13 @@
+//
+//  FreightDetailCustomCell.h
+//  FreteList
+//
+//  Created by Joao Alves on 2/23/15.
+//  Copyright (c) 2015 FreteList. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface FreightDetailCustomCell : PFTableViewCell
+
+@end
---
+++ FreteList/FreightDetailCustomCell.m
@@ -0,0 +1,21 @@
+//
+//  FreightDetailCustomCell.m
+//  FreteList
+//
+//  Created by Joao Alves on 2/23/15.
+//  Copyright (c) 2015 FreteList. All rights reserved.
+//
+
+#import "FreightDetailCustomCell.h"
+
+@implementation FreightDetailCustomCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
