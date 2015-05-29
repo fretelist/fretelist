7f88291682b8d6f16687604cb87ebddccf1e4584
joaopaulogalvao <joaopaulogalvao1@gmail.com>
2015-05-28 23:03:16 -0700

    Uncheck adjust Scroll View  insets on storyboard and Resize View From NIB on FreightLoginViewController.

--- FreteList/FreightLoginViewController.m
+++ FreteList/FreightLoginViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FreightLoginViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface FreightLoginViewController ()
 
@@ -17,6 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Set the navigation hidden
+    self.navigationController.navigationBarHidden = NO;
+    
+    self.scrFreightLogin.scrollEnabled = YES;
+    
+    // Set textFields delegates
+    self.txtFieldFreightLogin.delegate = self;
+    self.txtFieldFreightPsswdLogin.delegate = self;
+    
+    // Set TapGesture
+    self.gestureRecognizerFreightLogin = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(freightLoginHideKeyboard:)];
+    
+    [self.scrFreightLogin addGestureRecognizer:self.gestureRecognizerFreightLogin];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,12 +51,56 @@
 }
 */
 
+#pragma mark - My Actions
+// Hide Keyboard when Tapped
+-(void) freightLoginHideKeyboard: (id) sender {
+    
+    //Hide Keyboard if txtFieldFreightLogin
+    [self.txtFieldFreightLogin resignFirstResponder];
+    
+    //Set Scroll View Position
+    [self.scrFreightLogin setContentOffset:CGPointMake(0.0, 0.0) animated:YES];
+    
+    //Hide Keyboard if txtFieldPsswd
+    [self.txtFieldFreightPsswdLogin resignFirstResponder];
+    
+    //Set Scroll View Position
+    [self.scrFreightLogin setContentOffset:CGPointMake(0.0, 0.0) animated:YES];
+    
+    
+}
+
+
 - (IBAction)btnForgotPsswdFreightLogin:(id)sender {
+    
+    
 }
 
 - (IBAction)btnCancelFreightLogin:(id)sender {
+    
+    //Cancel Login
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (IBAction)btnPerformFreightLogin:(id)sender {
 }
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
